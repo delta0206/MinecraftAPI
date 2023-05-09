355143832c3d277f50ec3db421e33ca8df4b33d8
delta0206 <delta.gammatrees@gmail.com>
2023-05-09 22:44:26 +0900

    ✨ create recipe endpoints

---
+++ app/api/java/[version]/recipe/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server'
+
+import { validateVersion } from '@/_libs/validation'
+
+export const runtime = 'edge'
+
+export async function GET(req: Request, { version }: { version: string }) {
+  version = validateVersion(version)
+
+  // TODO: return a list of Java recipe endpoints
+  return NextResponse.json({
+    msg: `There is no information yet about recipes for Java ${version}. I would be happy if you could add what you know to https://github.com/delta0206/MinecraftAPI`,
+  })
+}
