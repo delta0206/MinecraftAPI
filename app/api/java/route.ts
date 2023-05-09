54dc62d6b47841939fa60e45287f8c1dcad49e14
delta0206 <delta.gammatrees@gmail.com>
2023-05-09 22:41:05 +0900

    ✨ create endpoint for `/api/java`

--- app/api/java/route.ts
+++ app/api/java/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from 'next/server'
 
+import { MinecraftVersions } from '@/_libs/minecraft'
+
 export const runtime = 'edge'
 
 export async function GET() {
-  // TODO: return endpoint for list of Java versions
   return NextResponse.json({
-    msg: 'There is no information yet about Java. I would be happy if you could add what you know to https://github.com/delta0206/MinecraftAPI',
+    endpoints: MinecraftVersions.map((version) => ({
+      name: version,
+      url: `${process.env.NEXT_PUBLIC_ROOT_URL}/api/java/${version}`,
+    })),
   })
 }
